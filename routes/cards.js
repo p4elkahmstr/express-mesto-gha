215a279cf08c7bd81194f02a4ea5db22635c66f2
Aiza Savvinova <hmstrayza@yandex.ru>
2023-08-09 22:18:41 +0600

    added body to Joi

--- routes/cards.js
+++ routes/cards.js
@@ -13,8 +13,10 @@ router.delete('/:cardId', celebrate({
 }), deleteCard);
 
 router.post('/', celebrate({
-  name: Joi.string().required().min(2).max(30),
-  link: Joi.string().required(),
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }),
 }), createCard);
 
 router.put('/:cardId/likes', celebrate({
