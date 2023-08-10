56e4d24d100f4cc0c7b2db8849902b9fee67fa4b
Aiza Savvinova <hmstrayza@yandex.ru>
2023-08-10 19:35:18 +0600

    fix auth.js UnauthorizedError

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,15 +4,14 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
-  let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key', { expiresIn: '7d' });
+    const payload = jwt.verify(token, 'some-secret-key', { expiresIn: '7d' });
+    req.user = payload;
+    return next();
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
-  req.user = payload;
-  return next();
 };
