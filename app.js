4b41d819b63a09db3014cc85768dafa61f9f65e6
Aiza Savvinova <hmstrayza@yandex.ru>
2023-08-10 16:50:54 +0600

    fix app express

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(helmet());
 
 app.use(express.json());
-app.use(express.json().urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
